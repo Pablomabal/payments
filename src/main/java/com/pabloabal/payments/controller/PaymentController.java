d0e2e1c7fe51ee4c7efc723adb6f0cb2088c29a3
Pablo Abal <29985160+Pablomabal@users.noreply.github.com>
2024-11-15 13:17:02 -0300

    Remove unused method

--- src/main/java/com/pabloabal/payments/controller/PaymentController.java
+++ src/main/java/com/pabloabal/payments/controller/PaymentController.java
@@ -1,16 +1,13 @@
 package com.pabloabal.payments.controller;
 
-import com.pabloabal.payments.dto.*;
-import com.pabloabal.payments.entity.Payment;
-import com.pabloabal.payments.entity.PaymentStatus;
+import com.pabloabal.payments.dto.CreatePaymentResultDTO;
+import com.pabloabal.payments.dto.PaymentDTO;
 import com.pabloabal.payments.service.kafka.PaymentsProducer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.Date;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/payments")
@@ -28,16 +25,4 @@ public class PaymentController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public ResponseEntity<Payment> test() {
-        var request = new TransferPaymentDTO();
-        request.setType("asd");
-        request.setAmount(BigDecimal.ONE);
-        request.setCurrency("USD");
-        request.setRate(BigDecimal.ONE);
-        request.setStatus(PaymentStatus.COMPLETED);
-        request.setCreatedAt(Date.from(Instant.now()));
-        producer.sendMessage(request);
-        return ResponseEntity.ok().build();
-    }
 }
